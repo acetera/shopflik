74eda17edeef9cfd6572238bdc5434950c72a130
ruby -v <alessandro.cetera@gmail.com>
2014-07-29 20:01:47 -0400

    commit with user authenticated root

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
 
 	def index
-		@products = Product.all
+		
 	end
 
+	def shop
+		@products = Product.all
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :products
 
-root "welcome#index"
+authenticated :user do
+  root :to => 'welcome#shop', as: :authenticated_root
+end
+
+root to: 'welcome#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
