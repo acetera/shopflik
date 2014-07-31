55bae1c61499a80b001819868b7ab09b98443dd1
ruby -v <alessandro.cetera@gmail.com>
2014-07-30 20:12:26 -0400

    commit with buttons

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'devise'
 gem 'bootstrap-sass', '~> 3.2.0'
+gem 'omniauth-facebook'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,14 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {   
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+        });
